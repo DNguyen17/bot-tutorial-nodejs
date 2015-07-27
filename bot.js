13e0b5120f821b8900cfe822c9887ad56e10e99f
dnguyen17 <danhn@smu.edu>
2015-07-27 09:48:42 +0000

    fixed made this

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ function respond() {
       botRegex6 = /(mlh)|(MLH)|([mM]ajor [Ll]eague [Hh]acking)/,
       botRegex7 = /[Ss]tickers/,
       botRegex8 = /[Ss]now/,
-      botRegex9 = /[Mm]ade ((it)|((is)|(at)))/,
+      botRegex9 = /[Mm]ade ((it)|(th(is)|(at)))/,
       botRegex10 = /[tT]he [bB]est/,
       botRegex11 = /[Bb]eep beep/;
 
